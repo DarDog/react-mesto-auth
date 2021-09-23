4cc6e370c3a1dbba08ed338dc15d754e6bb88ba3
DarDog <samediacc666@gmial.com>
2021-09-23 15:16:44 +1100

    Complete state logic for popups

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,19 @@
 import React from "react";
 
-function Main() {
-  const handleEditProfileClick = () => {
-    const editPopupElement = document.querySelector('.pop-up_content_edit');
-
-      editPopupElement.classList.add('pop-up_opened');
-  }
-
-  const handleAddPlaceClick = () => {
-    const addPopupElement = document.querySelector('.pop-up_content_add');
-
-      addPopupElement.classList.add('pop-up_opened')
-  }
-
-  const handleEditAvatarClick = () => {
-    const editAvatarPopupElement = document.querySelector('.pop-up_content_avatar');
-
-    editAvatarPopupElement.classList.add('pop-up_opened')
-  }
-
+function Main(props) {
 
   return (
       <main className="main page__main">
         <section className="profile main__profile">
-          <div onClick={handleEditAvatarClick} className="profile__overlay">
+          <div onClick={props.onEditAvatar} className="profile__overlay">
             <img src="#" alt="аватар пользователя" className="profile__avatar"/>
           </div>
           <div className="profile__info">
             <h1 className="profile__title">Жак-ив Кусто</h1>
-            <button onClick={handleEditProfileClick} type="button" className="profile__edit-button"></button>
+            <button onClick={props.onEditProfile} type="button" className="profile__edit-button"> </button>
             <p className="profile__subtitle">Исследователь океана</p>
           </div>
-          <button onClick={handleAddPlaceClick} type="button" className="profile__add-button"></button>
+          <button onClick={props.onAddPlace} type="button" className="profile__add-button"> </button>
         </section>
         <section className="elements main__elements">
           <ul className="elements__cards">
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,15 +2,21 @@ import React from "react";
 
 function PopupWithForm(props) {
 
+  const closeByClickAtOverlay = (e) => {
+    if (e.target.classList.contains('pop-up')) {
+      props.onClose()
+    }
+  }
+
+
   return (
-      <article className={`pop-up pop-up_content_${props.name}`}>
+      <article onClick={closeByClickAtOverlay} className={`pop-up pop-up_content_${props.name} ${props.isOpen && 'pop-up_opened'}`}>
         <div className="pop-up__background">
           <h2 className="pop-up__title">{props.title}</h2>
           <form noValidate className="form" name="editForm">
             {props.children}
-            <button type="submit" className="form__submit-button" name={`${props.name}`}>Сохранить</button>
           </form>
-          <button type="button" className="pop-up__exit-button"></button>
+          <button onClick={props.onClose} type="button" className="pop-up__exit-button"> </button>
         </div>
       </article>
   )
