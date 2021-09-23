42c411fb63c37b8db7fcbc1ad95b622c64efbe1c
DarDog <samediacc666@gmial.com>
2021-09-23 18:37:53 +1100

    Complete state logic for User info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,34 @@
 import React from "react";
+import {api} from "../utils/Api";
 
 function Main(props) {
+
+  const [userName, setUserName] = React.useState(''),
+        [userDescription, setUserDescription] = React.useState(''),
+        [userAvatar, setUserAvatar] = React.useState('');
+
+  React.useEffect(() => {
+    api.getUserInfo()
+        .then((userInfo) => {
+          setUserName(userInfo.name);
+          setUserDescription(userInfo.about);
+          setUserAvatar(userInfo.avatar)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+  })
 
   return (
       <main className="main page__main">
         <section className="profile main__profile">
           <div onClick={props.onEditAvatar} className="profile__overlay">
-            <img src="#" alt="аватар пользователя" className="profile__avatar"/>
+            <img src={userAvatar} alt="аватар пользователя" className="profile__avatar"/>
           </div>
           <div className="profile__info">
-            <h1 className="profile__title">Жак-ив Кусто</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button onClick={props.onEditProfile} type="button" className="profile__edit-button"> </button>
-            <p className="profile__subtitle">Исследователь океана</p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
           <button onClick={props.onAddPlace} type="button" className="profile__add-button"> </button>
         </section>
