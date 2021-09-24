ad2ce8bfc7da78cf16278abb7202c9d69e8e40cf
DarDog <samediacc666@gmial.com>
2021-09-24 19:09:12 +1100

    Fix: refactor PopupWithForm component and children props for correct complete

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -15,6 +15,7 @@ function PopupWithForm(props) {
           <h2 className="pop-up__title">{props.title}</h2>
           <form noValidate className="form" name="editForm">
             {props.children}
+            <button type="submit" className="form__submit-button" name="submitButton">{props.buttonText}</button>
           </form>
           <button onClick={props.onClose} type="button" className="pop-up__exit-button"> </button>
         </div>
