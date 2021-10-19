893317d8ffa015caeb56f82c9b6fd5e9354190fd
DarDog <samediacc666@gmial.com>
2021-10-19 15:53:48 +1100

    Feat: add form validation for Sign up and Sign in

--- src/components/auth/SignPage.js
+++ src/components/auth/SignPage.js
@@ -1,14 +1,77 @@
 import React from 'react';
 
 function SignPage({context, ...props}) {
+  const [login, setLogin] = React.useState(''),
+      [isLoginValid, setIsLoginValid] = React.useState(false),
+      [loginErrorMassage, setLoginErrorMassage] = React.useState(''),
+      [password, setPassword] = React.useState(''),
+      [isPasswordValid, setIsPasswordValid] = React.useState(false),
+      [passwordErrorMassage, setPasswordErrorMassage] = React.useState(''),
+      [isFormValid, setIsFormValid] = React.useState(false)
+
+  console.log(login)
+
+  const handleLoginChange = (e) => {
+    setLogin(e.target.value);
+    checkLoginValidity(e.target);
+  }
+
+  const checkLoginValidity = (input) => {
+    if (!input.validity.valid) {
+      setIsLoginValid(false);
+      setLoginErrorMassage(input.validationMessage);
+    } else {
+      setIsLoginValid(true);
+      setLoginErrorMassage('');
+    }
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+    checkPasswordValidity(e.target)
+  }
+
+  const checkPasswordValidity = (input) => {
+    if (!input.validity.valid) {
+      setIsPasswordValid(false);
+      setPasswordErrorMassage(input.validationMessage);
+    } else {
+      setIsPasswordValid(true);
+      setPasswordErrorMassage('');
+    }
+  }
+
+  React.useEffect(() => {
+    if (isLoginValid && isPasswordValid) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false)
+    }
+  },[login, password])
+
   return (
       <>
         <h1 className='auth__title'>{context.title}</h1>
-        <form className='auth__form' name={context.formName}>
-          <input className='form__input form__input_type_auth' minLength='2' placeholder='Email' type="email" required/>
-          <input className='form__input form__input_type_auth' minLength='2' placeholder='Пароль' type="password"
-                 required/>
-          <button className='form__submit-button form__submit-button_type_auth' type='submit'>{context.buttonName}</button>
+        <form className='auth__form' name={context.formName} noValidate>
+          <input className='form__input form__input_type_auth'
+                 minLength="6"
+                 placeholder='Email'
+                 type="email"
+                 required
+                 value={login}
+                 onChange={handleLoginChange}
+          />
+          <span className={`form__input-error ${isLoginValid || 'form__input-error_active'}`}>{loginErrorMassage}</span>
+          <input className='form__input form__input_type_auth'
+                 minLength='6'
+                 placeholder='Пароль'
+                 type="password"
+                 required
+                 value={password}
+                 onChange={handlePasswordChange}
+          />
+          <span className={`form__input-error ${isPasswordValid || 'form__input-error_active'}`}>{passwordErrorMassage}</span>
+          <button className={`form__submit-button form__submit-button_type_auth ${isFormValid || 'form__submit-button_disable'}`} type='submit'>{context.buttonName}</button>
         </form>
       </>
   );
